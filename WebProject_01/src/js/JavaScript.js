5d56eaf92fc40eb7723bfbbfbe76cdefda0dce5f
Danah-Mansour <98228060+Danah-Mansour@users.noreply.github.com>
2023-05-06 12:41:14 +0300

    1

---
+++ WebProject_01/src/js/JavaScript.js
@@ -0,0 +1,10 @@
+
+
+// Display some data from the object:
+//document.getElementById("CopyWriteww").innerHTML = "All Rights Reserved to Al-Yamamah University.Copyright 2023";
+window.onload = function (){
+ //document.getElementById("MyId").innerHTML = "Date : " + Date();
+ 
+ document.getElementById("CopyWrite").innerHTML = "All Rights Reserved to Al-Yamamah University.Copyright 2023";
+
+};
