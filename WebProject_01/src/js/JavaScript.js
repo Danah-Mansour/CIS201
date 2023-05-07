90121aeaf7e0f1502575b447846851b2e1416afd
Danah-Mansour <98228060+Danah-Mansour@users.noreply.github.com>
2023-05-07 20:23:22 +0300

    1

--- WebProject_01/src/js/JavaScript.js
+++ WebProject_01/src/js/JavaScript.js
@@ -8,4 +8,4 @@ window.onload = function (){
  document.getElementById("CopyWrite").innerHTML = "All Rights Reserved to Al-Yamamah University.Copyright 2023";
 
 };
-<script src="../src/js/JavaScript.js"></script> Products Display code + Cart counter>
+// <script src="../src/js/JavaScript.js"></script> Products Display code + Cart counter>
